329d3fd631981601fe0933532927c1eaec5b9910
Mafrans <malte.kluft@gmail.com>
2022-11-06 10:30:44 +0100

    feat: add collision

--- Assets/2 - Scripts/CameraFollow.cs
+++ Assets/2 - Scripts/CameraFollow.cs
@@ -7,23 +7,36 @@ public class CameraFollow : MonoBehaviour
     public Player player;
     public bool shaking = false;
     public float shakePower = 0.1f;
+    public Transform fixedPoint = null;
     private Camera camera;
     private Vector2 shakeOffset = Vector2.zero;
+    private Vector3 position;
     // Start is called before the first frame update
     void Start()
     {
         this.camera = GetComponent<Camera>();
+        this.position = transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.position = player.transform.position + Vector3.up * 20 + Vector3.forward * -10;
-        transform.position += transform.right * shakeOffset.x + transform.up * shakeOffset.y;
-
         if (shaking)
         {
-            shakeOffset = Random.Range(-1, 1) * shakePower * Vector2.one;
+            shakeOffset.x = Random.Range(-1, 1) * shakePower;
+            shakeOffset.y = Random.Range(-1, 1) * shakePower;
         }
+
+        if (fixedPoint != null)
+        {
+            position = Vector3.Lerp(position, fixedPoint.position, Time.deltaTime);
+            transform.rotation = Quaternion.Lerp(transform.rotation, fixedPoint.rotation, Time.deltaTime);
+        }
+        else
+        {
+            position = player.transform.position + Vector3.up * 20 + Vector3.forward * -10;
+        }
+
+        transform.position = position + transform.right * shakeOffset.x + transform.up * shakeOffset.y;
     }
 }
