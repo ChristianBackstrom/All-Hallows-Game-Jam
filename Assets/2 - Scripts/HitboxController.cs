bc97736d54bcd46795a1c33da1092caf6e2df3f0
ChristianBackstrom <christian.berggren.backstrom@gmail.com>
2022-11-06 10:28:05 +0100

    Can Die and have Charge Attack now

--- Assets/2 - Scripts/HitboxController.cs
+++ Assets/2 - Scripts/HitboxController.cs
@@ -1,14 +1,20 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class HitboxController : MonoBehaviour
 {
+	[SerializeField] private int _damage = 3;
 	private void OnTriggerEnter(Collider other)
 	{
 		if (!other.CompareTag("Player")) return;
 
-		other.GetComponent<Stats>().TakeDamage(2);
+		other.GetComponent<Stats>().TakeDamage(_damage);
+	}
+
+	private void OnCollisionEnter(Collision other)
+	{
+		if (!other.gameObject.CompareTag("Player")) return;
+
+		other.gameObject.GetComponent<Stats>().TakeDamage(_damage);
 	}
 
 	private void OnDrawGizmos()
