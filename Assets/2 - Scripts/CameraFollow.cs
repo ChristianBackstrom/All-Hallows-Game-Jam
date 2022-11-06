1db280c2dfbc61575bc1278daac6b2958c6b1be9
Mafrans <malte.kluft@gmail.com>
2022-11-06 10:11:10 +0100

    feat: working but shitty cutscene

--- Assets/2 - Scripts/CameraFollow.cs
+++ Assets/2 - Scripts/CameraFollow.cs
@@ -5,7 +5,10 @@ using UnityEngine;
 public class CameraFollow : MonoBehaviour
 {
     public Player player;
+    public bool shaking = false;
+    public float shakePower = 0.1f;
     private Camera camera;
+    private Vector2 shakeOffset = Vector2.zero;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,5 +19,11 @@ public class CameraFollow : MonoBehaviour
     void Update()
     {
         transform.position = player.transform.position + Vector3.up * 20 + Vector3.forward * -10;
+        transform.position += transform.right * shakeOffset.x + transform.up * shakeOffset.y;
+
+        if (shaking)
+        {
+            shakeOffset = Random.Range(-1, 1) * shakePower * Vector2.one;
+        }
     }
 }
